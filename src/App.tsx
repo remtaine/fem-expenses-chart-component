161a61dc87573f3bc8887d9df4b8674efb729eef
Niel Benjamin Kho <benjokho@gmail.com>
2022-08-10 19:16:01 +0800

    Remove template files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Balance from "./components/Balance";
+import Graph from "./components/Graph";
+import Spending from "./components/Spending";
+import Total from "./components/Total";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-screen min-h-screen flex items-center justify-center bg-fem-500 font-dm-sans selection:bg-neutral-800 selection:text-neutral-50">
+      <section className="flex flex-col gap-4 w-[350px]">
+        <div className="bg-fem-100 p-4 rounded-lg">
+          <Balance />
+        </div>
+        <div className="bg-fem-600 p-4 rounded-lg flex flex-col gap-2">
+          <Spending />
+
+          <div className="h-[1px] bg-fem-100 opacity-10"></div>
+
+          <Total />
+        </div>
+      </section>
     </div>
   );
 }
